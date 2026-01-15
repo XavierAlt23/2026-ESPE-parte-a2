8ac1cc25ca40d795b863bcfcca8f0093a93f82e1
odieste <odieste@fi.upm.es>
2026-01-15 10:39:35 +0100

    Initial commit

---
+++ src/main/java/es/grise/upm/profundizacion/subscriptionService/SubscriptionService.java
@@ -0,0 +1,33 @@
+package es.grise.upm.profundizacion.subscriptionService;
+
+import java.util.Collection;
+
+public class SubscriptionService {
+
+	private Collection <User> subscribers;
+	
+	/*
+	 * Constructor
+	 */
+	public SubscriptionService() {}
+	
+	
+
+	/*
+	 * Method to code / test
+	 */
+	public void addSubscriber(User user) {}
+	
+	
+	
+	
+	/*
+	 * Other setters & getters
+	 */
+	public Collection <User> getSubscribers() {
+		
+		return subscribers;
+		
+	}
+
+}
